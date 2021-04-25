dfb9b0b120751d2755a980c15dc218f51ca8f47b
jb josselin <jeanbaptistejosselin@gmail.com>
2021-04-25 17:11:16 +0200

    first commit

---
+++ src/components/Post/LikeButton.jsx
@@ -0,0 +1,50 @@
+import React, { useContext, useEffect, useState } from "react";
+import { UidContext } from "../AppContext";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+import { useDispatch } from "react-redux";
+import { likePost, unlikePost } from "../../actions/post.actions";
+
+const LikeButton = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+  const uid = useContext(UidContext);
+  const dispatch = useDispatch();
+
+  const like = () => {
+    dispatch(likePost(post._id, uid));
+    setLiked(true);
+  };
+
+  const unLike = () => {
+    dispatch(unlikePost(post._id, uid));
+    setLiked(false);
+  };
+
+  useEffect(() => {
+    if (post.likers.includes(uid)) setLiked(true);
+    else setLiked(false);
+  }, [uid, post.likers, liked]);
+
+  return (
+    <div className="like-container">
+      {uid === null && (
+        <Popup
+          trigger={<img src="./img/icons/heart.svg" alt="like" />}
+          position={["bottom center", "bottom right", "bottom left"]}
+          closeOnDocumentClick
+        >
+          <div>Connectez-vous pour liker ce post !</div>
+        </Popup>
+      )}
+      {uid && liked === false && (
+        <img src="./img/icons/heart.svg" alt="like" onClick={like} />
+      )}
+      {uid && liked && (
+        <img src="./img/icons/heart-filled.svg" alt="unlike" onClick={unLike} />
+      )}
+      <span>{post.likers.length}</span>
+    </div>
+  );
+};
+
+export default LikeButton;
---
+++ src/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import "./styles/index.scss";
+import { getUsers } from "./actions/users.actions";
+import { getPosts } from "./actions/post.actions";
+
+// dev tools
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+store.dispatch(getUsers());
+store.dispatch(getPosts());
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
